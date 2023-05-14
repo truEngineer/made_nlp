ff34a0a55f5cbf33a7bd8de7f37dcbba6aacbfdd
truEngineer <62888379+truEngineer@users.noreply.github.com>
2023-05-14 22:15:15 +0300

    Add gru_attention.py

---
+++ nmt/models/gru_attention.py
@@ -0,0 +1,101 @@
+import random
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, emb_dim, enc_hid_dim, dec_hid_dim, dropout):
+        super().__init__()
+        self.input_dim = input_dim
+        self.emb_dim = emb_dim
+        self.enc_hid_dim = enc_hid_dim
+        self.dec_hid_dim = dec_hid_dim
+        self.embedding = nn.Embedding(num_embeddings=input_dim, embedding_dim=emb_dim)
+        self.rnn = nn.GRU(input_size=emb_dim, hidden_size=enc_hid_dim, bidirectional=True)
+        self.fc = nn.Linear(in_features=enc_hid_dim * 2, out_features=dec_hid_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, src):
+        embedded = self.dropout(self.embedding(src))
+        outputs, hidden = self.rnn(embedded)
+        hidden = torch.tanh(self.fc(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1)))
+        return outputs, hidden
+
+
+class Attention(nn.Module):
+    def __init__(self, enc_hid_dim, dec_hid_dim, attn_dim):
+        super().__init__()
+        self.enc_hid_dim = enc_hid_dim
+        self.dec_hid_dim = dec_hid_dim
+        self.attn_in = (enc_hid_dim * 2) + dec_hid_dim
+        self.attn = nn.Linear(in_features=self.attn_in, out_features=attn_dim)
+
+    def forward(self, decoder_hidden, encoder_outputs):
+        src_len = encoder_outputs.shape[0]
+        repeated_decoder_hidden = decoder_hidden.unsqueeze(1).repeat(1, src_len, 1)
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        energy = torch.tanh(self.attn(
+            torch.cat((repeated_decoder_hidden, encoder_outputs), dim=2)
+        ))
+        attention = torch.sum(energy, dim=2)
+        return F.softmax(attention, dim=1)
+
+
+class Decoder(nn.Module):
+    def __init__(self, output_dim, emb_dim, enc_hid_dim, dec_hid_dim, dropout, attention):
+        super().__init__()
+        self.emb_dim = emb_dim
+        self.enc_hid_dim = enc_hid_dim
+        self.dec_hid_dim = dec_hid_dim
+        self.output_dim = output_dim
+        self.attention = attention
+        self.embedding = nn.Embedding(num_embeddings=output_dim, embedding_dim=emb_dim)
+        self.rnn = nn.GRU(input_size=enc_hid_dim * 2 + emb_dim, hidden_size=dec_hid_dim)
+        self.out = nn.Linear(in_features=self.attention.attn_in + emb_dim, out_features=output_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def _weighted_encoder_rep(self, decoder_hidden, encoder_outputs):
+        a = self.attention(decoder_hidden, encoder_outputs)
+        a = a.unsqueeze(1)
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        weighted_encoder_rep = torch.bmm(a, encoder_outputs)
+        weighted_encoder_rep = weighted_encoder_rep.permute(1, 0, 2)
+        return weighted_encoder_rep
+
+    def forward(self, input, decoder_hidden, encoder_outputs):
+        input = input.unsqueeze(0)
+        embedded = self.dropout(self.embedding(input))
+        weighted_encoder_rep = self._weighted_encoder_rep(decoder_hidden, encoder_outputs)
+        rnn_input = torch.cat((embedded, weighted_encoder_rep), dim=2)
+        output, decoder_hidden = self.rnn(rnn_input, decoder_hidden.unsqueeze(0))
+        embedded = embedded.squeeze(0)
+        output = output.squeeze(0)
+        weighted_encoder_rep = weighted_encoder_rep.squeeze(0)
+        output = self.out(torch.cat((output, weighted_encoder_rep, embedded), dim=1))
+        return output, decoder_hidden.squeeze(0)
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+
+    def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        batch_size = src.shape[1]
+        max_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim
+        outputs = torch.zeros(max_len, batch_size, trg_vocab_size).to(self.device)
+        encoder_outputs, hidden = self.encoder(src)
+        # first input to the decoder is the <bos> token
+        output = trg[0, :]
+        for t in range(1, max_len):
+            output, hidden = self.decoder(output, hidden, encoder_outputs)
+            outputs[t] = output
+            teacher_force = random.random() < teacher_forcing_ratio
+            top1 = output.argmax(1)
+            output = (trg[t] if teacher_force else top1)
+        return outputs
